c0971f89fab265c69965708e0101b55f364de3b7
Toni <toninade10@gmail.com>
2025-10-07 11:53:27 +0300

    fix element shop/details.jsx

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,4 +1,4 @@
-import { Button, Container } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import "./details.css";
 import { useContextCart } from "./context/Maincontext";
 import Currency_fun from "./currency";
@@ -10,50 +10,55 @@ const Details = ({ id, img, title, price, stars, des }) => {
   return (
     <div className="Details">
       <Container className="d-flex ">
-        <div className="all_imgs">
-          <div className="img_cont">
-            <img src={img} alt={title} />
-          </div>
-          <div className="imgs_cont">
-            <img src={img} alt={title} />
-            <img src={img} alt={title} />
-            <img src={img} alt={title} />
-            <img src={img} alt={title} />
-          </div>
-        </div>
-        <div className="element-text">
-          <h1>{title}</h1>
-          <h5>
-            {" "}
-            RATE : {stars}
-            <i className="fa-solid fa-star"></i>
-          </h5>
-          <h5> PRICE :{Currency_fun(price)}</h5>
-          <p>
-            <span>DESCRUBTION :</span> {des}
-          </p>
+        <Row>
+          <Col sm={12} lg={4}>
+            <div className="all_imgs">
+              <div className="img_cont">
+                <img src={img} alt={title} />
+              </div>
+              <div className="imgs_cont">
+                <img src={img} alt={title} />
+                <img src={img} alt={title} />
+                <img src={img} alt={title} />
+                <img src={img} alt={title} />
+              </div>
+            </div>
+          </Col>
+          <Col sm={12} lg={8}>
+            <div className="element-text">
+              <h1>{title}</h1>
+              <h5>
+                {" "}
+                RATE : {stars}
+                <i className="fa-solid fa-star"></i>
+              </h5>
+              <h5> PRICE :{Currency_fun(price)}</h5>
+              <p>
+                <span>DESCRUBTION :</span> {des}
+              </p>
 
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt
-            vero non corrupti alias ab necessitatibus illum eos magni labore
-            praesentium qui dolor expedita voluptas, nihil, ipsa molestias
-            inventore quidem possimus dicta soluta ut cumque. Repellat saepe
-            omnis deleniti aperiam maxime?
-          </p>
-          <div className=" btns">
-            <Button onClick={() => addToCart(id)}>
-              ADD TO CART <FontAwesomeIcon icon={faCartShopping} />
-            </Button>
-            <Button
-              onClick={() => {
-                addTofavorite(id);
-              }}
-            >
-              ADD TO FAVORITE
-              <FontAwesomeIcon icon={faHeart} />
-            </Button>
-          </div>
-        </div>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Nesciunt vero non corrupti alias ab necessitatibus illum eos
+                magni labore praesentium qui dolor expedita voluptas, nihil,
+                ipsa molestias inventore quidem possimus dicta soluta ut cumque.
+                Repellat saepe omnis deleniti aperiam maxime?
+              </p>
+              <div className=" btns">
+                <Button onClick={() => addToCart(id)}>
+                  <FontAwesomeIcon icon={faCartShopping} /> ADD TO CART
+                </Button>
+                <Button
+                  onClick={() => {
+                    addTofavorite(id);
+                  }}
+                >
+                  <FontAwesomeIcon icon={faHeart} /> ADD TO FAVORITE
+                </Button>
+              </div>
+            </div>
+          </Col>
+        </Row>
       </Container>
     </div>
   );
