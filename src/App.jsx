03cb51d9a7b73e0bad1a2914a35c27130bea18f0
Toni <toninade10@gmail.com>
2025-12-13 15:23:20 +0200

    this

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,25 @@
 import "./sass/pages/main.scss";
 import "./sass/pages/media.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Home from "./pages/Home";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Shop from "./pages/Shop";
-import MainNavbar from "./components/header/MainNavbar";
-import Topselling from "./pages/Topselling";
-import Hotdeals from "./pages/Hotdeals";
-import About from "./pages/About";
-import Signup from "./pages/Signup";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
-import ElementDetails from "./components/ElementDetails";
 import "./sass/helpers/reset.scss";
-import ShopCart from "./pages/ShopCart";
+
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 import Maincontext from "./components/context/Maincontext";
-import PaymentForm from "./pages/payment";
+import MainNavbar from "./components/header/MainNavbar";
+
+const Home = lazy(() => import("./pages/Home"));
+const Shop = lazy(() => import("./pages/Shop"));
+const Topselling = lazy(() => import("./pages/Topselling"));
+const Hotdeals = lazy(() => import("./pages/Hotdeals"));
+const About = lazy(() => import("./pages/About"));
+const Signup = lazy(() => import("./pages/Signup"));
+const Register = lazy(() => import("./pages/Register"));
+const Login = lazy(() => import("./pages/Login"));
+const ShopCart = lazy(() => import("./pages/ShopCart"));
+const PaymentForm = lazy(() => import("./pages/payment"));
+const ElementDetails = lazy(() => import("./components/ElementDetails"));
 
 function App() {
   return (
@@ -23,20 +27,22 @@ function App() {
       <BrowserRouter basename="/e-commerce_new">
         <Maincontext>
           <MainNavbar />
-          <Routes>
-            <Route path="/shop" element={<Shop />} />
-            <Route path="/" element={<Home />} />
-            <Route path="*" element={<Home />} />
-            <Route path="/hotdeals" element={<Hotdeals />} />
-            <Route path="/topselling" element={<Topselling />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/sub" element={<Signup />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/opencart" element={<ShopCart />} />
-            <Route path="/elementdetails" element={<ElementDetails />} />
-            <Route path="/payment" element={<PaymentForm />} />
-          </Routes>
+          <Suspense fallback={<div>Loading ...</div>}>
+            <Routes>
+              <Route path="/shop" element={<Shop />} />
+              <Route path="/" element={<Home />} />
+              <Route path="*" element={<Home />} />
+              <Route path="/hotdeals" element={<Hotdeals />} />
+              <Route path="/topselling" element={<Topselling />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/sub" element={<Signup />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/opencart" element={<ShopCart />} />
+              <Route path="/elementdetails" element={<ElementDetails />} />
+              <Route path="/payment" element={<PaymentForm />} />
+            </Routes>
+          </Suspense>
         </Maincontext>
       </BrowserRouter>
     </>
