9213ed04460be860a67ede4108352dcda3059401
Toni <toninade10@gmail.com>
2025-12-22 13:30:08 +0200

    Updates

---
+++ assets/Badge-ueTe_MNw.js
@@ -0,0 +1,1 @@
+import{r as i,u as m,j as x,c}from"./index-DsRwTBGW.js";const l=i.forwardRef(({bsPrefix:r,bg:s="primary",pill:e=!1,text:a,className:o,as:t="span",...p},d)=>{const f=m(r,"badge");return x.jsx(t,{ref:d,...p,className:c(o,f,e&&"rounded-pill",a&&`text-${a}`,s&&`bg-${s}`)})});l.displayName="Badge";export{l as B};
